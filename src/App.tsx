931b3543de637b0589d8506db20d05db02f6c037
S-P-R <spr2354@gmail.com>
2024-07-25 13:48:09 -0400

    Add News Dashboard project to Projects section

--- src/App.tsx
+++ src/App.tsx
@@ -19,18 +19,16 @@ function App() {
         <div className='hero-section'> <HeroSection/> </div> 
         <div className='main-content'>
           <h3> Hello! &#128075;{/* waving hand emoji */} </h3> 
-          <p> I’m Sean, a fourth-year student at Tufts University &#128024;{/* elephant emoji */} pursuing a Bachelor’s
+          <p> I’m Sean, a recent graduate of Tufts University &#128024;{/* elephant emoji */} with a Bachelor’s
               in Computer Science. Currently looking for a new grad software 
               engineering role. </p>
-          <h2> Projects</h2>
-          <Project name='Placeholder 1' 
-                   description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pulvinar volutpat ante sit amet malesuada." 
-                   onMouseEnter={() => setHoveredProject("Placeholder 1")} onMouseLeave={() => setHoveredProject(null)} 
-                   shouldDim={hoveredProject != null && hoveredProject != "Placeholder 1"} />
-          <Project name='Placeholder 2' 
-                   description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pulvinar volutpat ante sit amet malesuada." 
-                   onMouseEnter={() => setHoveredProject("Placeholder 2")} onMouseLeave={() => setHoveredProject(null)}
-                   shouldDim={hoveredProject != null && hoveredProject != "Placeholder 2"} />
+          <h2> Projects </h2>
+          <Project name='News Dashboard' 
+                   description="A full-stack project consisting of a webscraper written in Python that gathers news headlines from various news
+                   websites' front pages, a Flask API that serves these headlines, and a React frontend that displays news summary information" 
+                   onMouseEnter={() => setHoveredProject("News Dashboard")} onMouseLeave={() => setHoveredProject(null)} 
+                   shouldDim={hoveredProject != null && hoveredProject != "News Dashboard"} />
+
         </div>
       </div>
       <ColorSchemeToggle/>
