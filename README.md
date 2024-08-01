49a6784f8a823597226c2b757cfde0b8e5826445
S-P-R <spr2354@gmail.com>
2024-07-31 23:34:26 -0400

    Add README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Personal Website
+
+## Description
+
+A simple personal website intended to list my most prominent skills and to
+showcase my deployed projects
+
+## Deployment
+
+The website is currently [deployed on github pages](https://s-p-r.github.io/)
--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,12 @@ function App() {
                 <div className='hero-section'> <HeroSection/> </div> 
                 <div className='main-content'>
                     <h3> Hello! &#128075;{/* waving hand emoji */} </h3> 
-                    <p> I’m Sean, a recent graduate of Tufts University <span className='elephant-emoji'>&#128024;</span>{/* elephant emoji */}
-                        with a Bachelor’sin Computer Science. Currently looking for a new grad software 
+                    <p> I’m Sean, a recent graduate of Tufts University <span className='elephant-emoji'>&#128024;</span> {/* elephant emoji */}
+                        with a Bachelor’s in Computer Science. Currently looking for a new grad software 
                         engineering role. </p>
                     <h2> Skills </h2>
                     <SkillsList/>
-                    <h2> Projects </h2>
+                    <h2> Deployed Projects </h2>
                     <Project name='News Dashboard' 
                             description="A full-stack project consisting of a webscraper written in Python that gathers news headlines from various news
                             websites' front pages, a Flask API that serves these headlines, and a React frontend that displays news summary information"
